a13bf5347aa5fd06e318bd49d53f17dbc5cace85
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2023-10-12 13:53:05 +0300

    26 gün

---
+++ src/day26_ovverriddin_pollyMorphism/C04_RahatChild.java
@@ -0,0 +1,14 @@
+package day26_ovverriddin_pollyMorphism;
+
+public class C04_RahatChild {
+
+    /*
+        klasik inheritance
+        tum insiyatifi child class a birakir
+
+        child class parent class daki class uyelerinden
+        1- istedigini ayni kullanilir
+        2- istedigini kendisine uyarlar
+        3- isterse de parent class da olmayan yeni uyeleri ekleyebilir
+     */
+}
