c4af91257719ff212ef8d0742dc6d73bc6713b50
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2023-10-16 13:56:02 +0300

    27 gün

---
+++ src/day27_abstractClasses_interfaces/C03_RahatChild.java
@@ -0,0 +1,8 @@
+package day27_abstractClasses_interfaces;
+
+import day26_ovverriddin_pollyMorphism.C03_ZorlamaSevmeyenParent;
+
+public class C03_RahatChild extends C03_ZorlamaSevmeyenParent {
+
+
+}
