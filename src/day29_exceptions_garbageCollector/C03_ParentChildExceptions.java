8865ee81438ec485ac501710b0ad7692ed2d128d
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2023-10-18 17:11:31 +0300

    29 gün

---
+++ src/day29_exceptions_garbageCollector/C03_ParentChildExceptions.java
@@ -0,0 +1,43 @@
+package day29_exceptions_garbageCollector;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C03_ParentChildExceptions {
+
+    public static void main(String[] args) {
+
+        /*
+           Birden fazla exception olusma ihtimali varsa
+           ve bu exceptin'lar arasinda Parent - Child iliskisi olursa
+
+           once Child, sonra Parent yazilmalidir.
+
+           Tersi yapilirsa
+           yani once Parent yazilirsa
+           Parenr exception'in yakalam kapasitesi daha fazla oldugundan
+           tum olasi exception'lari yakalayacak
+           ve altdaki Child exception'a is kalmayacaktir.
+           Boyle yazarsak java altini cizer
+           ve duzeltmemizi ister
+        */
+
+
+        try {
+            FileInputStream fis = new FileInputStream("src/day28_exceptions1/text.txt");
+
+            int k=0;
+            while((k= fis.read() ) != (-1)) {
+                System.out.print((char) k);
+            }
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Dosya yolu yanlis");
+
+        } catch (IOException e) {
+
+            System.out.println("Dosya okunamiyor");
+        }
+    }
+}
